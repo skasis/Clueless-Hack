b7106af23f3cfa9842d39bca24d5e46e1c3be858
Sophie Jones <s.a.jones72@gmail.com>
2014-10-04 16:12:05 +0200

    More SendGrid

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 from flask import Flask
 import simplejson
+import sendgrid
 app = Flask(__name__)
 
 @app.route('/parse', methods=('POST'))
-def sendgrid_parser(request):
+def sendgrid_parser(request):    
+    # Consume the entire email
+    envelope = simplejson.loads(request.form.get('envelope'))
+    
+    # Get some header information
+    to_address = envelope['to'][0]
+    from_address = envelope['from']
+    
+    # Now, onto the body
+    text = request.form.get('text')
+    html = request.form.get('html')
+    subject = request.form.get('subject')
+    
+    sg = sendgrid.SendGridClient('YOUR_SENDGRID_USERNAME', 'YOUR_SENDGRID_PASSWORD')
 
-      # Consume the entire email
-      envelope = simplejson.loads(request.form.get('envelope'))
-    
-      # Get some header information
-      to_address = envelope['to'][0]
-      from_address = envelope['from']
-    
-      # Now, onto the body
-      text = request.form.get('text')
-      html = request.form.get('html')
-      subject = request.form.get('subject')
-    
-      
-      print(subject)
-    
-      return "OK"
+    message = sendgrid.Mail()
+    message.add_to('John Doe <alexhygate@googlemail.com>')
+    message.set_subject(subject)
+    message.set_html('Body')
+    message.set_text('Body')
+    message.set_from('Doe John <doe@email.com>')
+    status, msg = sg.send(message)
+    return "OK"
   
 if __name__ == "__main__":
   app.run()
